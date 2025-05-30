cd32b69159ef7e343a8432b5f65a53238330fdb1
pedroeug <105739123+pedroeug@users.noreply.github.com>
2025-05-30 20:24:50 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-set -e
-
-echo "[1/3] Instalando dependências do gdown..."
-pip install gdown
-
-echo "[2/3] Baixando os modelos com gdown..."
-python download_models.py
-
-echo "[3/3] Iniciando o servidor..."
-exec uvicorn backend.main:app --host 0.0.0.0 --port $PORT
+uvicorn backend.main:app --host 0.0.0.0 --port $PORT
