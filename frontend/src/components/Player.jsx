df9530aa1a2673bebf5a0e2fd79a7fa3c7b43385
pedroeug <105739123+pedroeug@users.noreply.github.com>
2025-06-02 15:58:13 -0300

    Update Player.jsx

--- frontend/src/components/Player.jsx
+++ frontend/src/components/Player.jsx
@@ -1,1 +1,18 @@
+// frontend/src/components/Player.jsx
+
+import React from "react";
+
+const Player = ({ src, width = "640", height = "360" }) => (
+  <video
+    controls
+    width={width}
+    height={height}
+    style={{ backgroundColor: "#000" }}
+  >
+    <source src={src} type="application/vnd.apple.mpegURL" />
+    Seu navegador não suporta reprodução HLS.
+  </video>
+);
+
+export default Player;
 
