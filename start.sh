8b8450a799cd67811ac5b71ec2cafabe5c1f5aa7
pedroeug <105739123+pedroeug@users.noreply.github.com>
2025-06-02 19:52:04 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-
-# arquivo start.sh na raiz do projeto
-# Inicia o Uvicorn com FastAPI usando a porta 8000
-
-exec uvicorn backend.main:app --host 0.0.0.0 --port "${PORT:-8000}"
+uvicorn backend.main:app --host 0.0.0.0 --port $PORT
